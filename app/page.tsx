1d0c0c8d081c7c40dad2141584a8236b53c6225a
simonchan66 <formul529@gmail.com>
2024-01-25 12:30:46 -0700

    1st

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Image from "next/image";
+import { useDraw } from "@/hooks/useDraw";
+
+export default function Home() {
+  const {canvasRef, onMouseDown} = useDraw(drawLine);
+
+  function drawLine({prevPoint, currentPoint,ctx}:Draw) {
+    const {x:currX,y:currY} = currentPoint;
+    const color = "#000";
+    const lineWidth = 5;
+
+    let startPoint = prevPoint?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+    ctx.moveTo(startPoint.x,startPoint.y);
+    ctx.lineTo(currX,currY);
+    ctx.stroke();
+    ctx.fillStyle = color;
+
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y,2,0,Math.PI*2);
+    ctx.fill();
+  }
+
+
+
+  return (
+<div className="w-screen h-screen bg-white flex justify-center items-center">
+<canvas onMouseDown={onMouseDown} ref= {canvasRef} id="canvas" width={600} height={600}
+className="border border-black rounded-lg"  
+
+/>
+
+
+
+</div>
+
+
+  );
+}
